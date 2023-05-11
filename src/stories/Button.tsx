a39d0192f914a138baae5f3bce284785d369efc4
watanabe0055 <mailok1212+github@gmail.com>
2023-05-12 00:21:31 +0900

    fix:軽微な修正

--- src/stories/Button.tsx
+++ src/stories/Button.tsx
@@ -3,21 +3,14 @@ import "./button.css";
 
 interface ButtonProps {
   /**
-   * Is this the principal call to action on the page?
-   */
-  color: string;
-  /**
-   * What background color to use
-   */
-  backgroundColor?: string;
-  /**
-   * How large should the button be?
-   */
-  size?: "small" | "medium" | "large";
-  /**
-   * Button contents
+   * ボタン内の文字
    */
   label: string;
+  /**
+   * ボタン内の文字色
+   */
+  color: string;
+  size?: "small" | "medium" | "large";
   onClick?: () => void;
 }
 

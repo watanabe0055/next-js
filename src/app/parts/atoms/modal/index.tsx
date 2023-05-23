90d983627383dd063cfab1c2fc3e5a0f7852aaa8
watanabe0055 <mailok1212+github@gmail.com>
2023-05-24 00:12:09 +0900

    fix:しゅせい

--- src/app/parts/atoms/modal/index.tsx
+++ src/app/parts/atoms/modal/index.tsx
@@ -4,20 +4,21 @@ import { useRecoilState } from "recoil";
 
 export const TaskModal = () => {
   const [isModal, setIsModal] = useRecoilState(modalFlagState);
+  /**モーダルを閉じる */
   const handleClickModal = () => {
     setIsModal(!isModal);
   };
 
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center">
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-yellow-300 opacity-50">
       <div className="p-6 bg-white rounded-lg">
-        <h2 className="mb-4 text-xl font-bold">Modal Title</h2>
-        <p className="mb-4">Modal content goes here.</p>
+        <h2 className="mb-4 text-xl font-bold">タスク</h2>
+        <p className="mb-4">登録するタスクを入力してください</p>
         <button
           className="px-4 py-2 text-white bg-gray-500 rounded"
           onClick={handleClickModal}
         >
-          Close
+          登録
         </button>
       </div>
     </div>

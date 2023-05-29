983d18efd722bba24d36ceaa96392dade4a1839e
watanabe0055 <mailok1212+github@gmail.com>
2023-05-30 00:31:12 +0900

    fix: リファクタリング

--- src/app/parts/atoms/modal/index.tsx
+++ src/app/parts/atoms/modal/index.tsx
@@ -69,115 +69,112 @@ export const TaskModal = () => {
 
   return (
     <>
-      {isUpdateFlag ? (
-        <div
-          className="fixed inset-0 z-50 flex items-center justify-center bg-yellow-300"
-          style={{ background: "rgba(252, 211, 77, 0.6)" }}
-        >
-          <div className="p-6 bg-white rounded-lg">
-            <div className="flex items-center justify-between mb-4">
-              <h2 className="text-xl font-bold">タスク更新</h2>
-              <button
-                className="px-4 py-2 text-white bg-red-500 rounded "
-                onClick={handleClickModal}
-              >
-                閉じる
-              </button>
-            </div>
-            <p className="mb-4">更新するタスクを入力してください</p>
-            <form onSubmit={handleSubmit(handleUpdateSubmit)}>
-              <AppContainer>
-                <label
-                  htmlFor="my-input"
-                  className="mb-1 font-medium text-gray-700 "
+      <div
+        className="fixed inset-0 z-50 flex items-center justify-center bg-yellow-300"
+        style={{ background: "rgba(252, 211, 77, 0.6)" }}
+      >
+        <div className="p-6 bg-white rounded-lg">
+          {isUpdateFlag ? (
+            <>
+              <div className="flex items-center justify-between mb-4">
+                <h2 className="text-xl font-bold">タスク更新</h2>
+                <button
+                  className="px-4 py-2 text-white bg-red-500 rounded "
+                  onClick={handleClickModal}
                 >
-                  タスク概要
-                </label>
-                <input
-                  {...register("taskTitle", { required: true })}
-                  defaultValue={tasks[selectedTaskId].title}
-                  className="w-full min-w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-                />
-                {errors.taskTitle && (
-                  <p className="text-red-400">必須項目です</p>
-                )}
-              </AppContainer>
-              <AppContainer>
-                <label
-                  htmlFor="my-input"
-                  className="mb-1 font-medium text-gray-700"
+                  閉じる
+                </button>
+              </div>
+              <p className="mb-4">更新するタスクを入力してください</p>
+              <form onSubmit={handleSubmit(handleUpdateSubmit)}>
+                <AppContainer>
+                  <label
+                    htmlFor="my-input"
+                    className="mb-1 font-medium text-gray-700 "
+                  >
+                    タスク概要
+                  </label>
+                  <input
+                    {...register("taskTitle", { required: true })}
+                    defaultValue={tasks[selectedTaskId].title}
+                    className="w-full min-w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  />
+                  {errors.taskTitle && (
+                    <p className="text-red-400">必須項目です</p>
+                  )}
+                </AppContainer>
+                <AppContainer>
+                  <label
+                    htmlFor="my-input"
+                    className="mb-1 font-medium text-gray-700"
+                  >
+                    タスク内容
+                  </label>
+                  <input
+                    {...register("taskContent", { required: true })}
+                    defaultValue={tasks[selectedTaskId].content}
+                    className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  />
+                  {errors.taskContent && (
+                    <p className="text-red-400">必須項目です</p>
+                  )}
+                </AppContainer>
+                <button className="px-4 py-2 text-white bg-gray-500 rounded">
+                  更新
+                </button>
+              </form>
+            </>
+          ) : (
+            <>
+              <div className="flex items-center justify-between mb-4">
+                <h2 className="text-xl font-bold">タスク</h2>
+                <button
+                  className="px-4 py-2 text-white bg-red-500 rounded "
+                  onClick={handleClickModal}
                 >
-                  タスク内容
-                </label>
-                <input
-                  {...register("taskContent", { required: true })}
-                  defaultValue={tasks[selectedTaskId].content}
-                  className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-                />
-                {errors.taskContent && (
-                  <p className="text-red-400">必須項目です</p>
-                )}
-              </AppContainer>
-              <button className="px-4 py-2 text-white bg-gray-500 rounded">
-                更新
-              </button>
-            </form>
-          </div>
+                  閉じる
+                </button>
+              </div>
+              <p className="mb-4">登録するタスクを入力してください</p>
+              <form onSubmit={handleSubmit(onCreateSubmit)}>
+                <AppContainer>
+                  <label
+                    htmlFor="my-input"
+                    className="mb-1 font-medium text-gray-700 "
+                  >
+                    タスク概要
+                  </label>
+                  <input
+                    {...register("taskTitle", { required: true })}
+                    className="w-full min-w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  />
+                  {errors.taskTitle && (
+                    <p className="text-red-400">必須項目です</p>
+                  )}
+                </AppContainer>
+                <AppContainer>
+                  <label
+                    htmlFor="my-input"
+                    className="mb-1 font-medium text-gray-700"
+                  >
+                    タスク内容
+                  </label>
+                  <input
+                    {...register("taskContent", { required: true })}
+                    className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  />
+                  {errors.taskContent && (
+                    <p className="text-red-400">必須項目です</p>
+                  )}
+                </AppContainer>
+                <button className="px-4 py-2 text-white bg-gray-500 rounded">
+                  登録
+                </button>
+              </form>
+            </>
+          )}
         </div>
-      ) : (
-        <div
-          className="fixed inset-0 z-50 flex items-center justify-center bg-yellow-300"
-          style={{ background: "rgba(252, 211, 77, 0.6)" }}
-        >
-          <div className="p-6 bg-white rounded-lg">
-            <div className="flex items-center justify-between mb-4">
-              <h2 className="text-xl font-bold">タスク</h2>
-              <button
-                className="px-4 py-2 text-white bg-red-500 rounded "
-                onClick={handleClickModal}
-              >
-                閉じる
-              </button>
-            </div>
-            <p className="mb-4">登録するタスクを入力してください</p>
-            <form onSubmit={handleSubmit(onCreateSubmit)}>
-              <AppContainer>
-                <label
-                  htmlFor="my-input"
-                  className="mb-1 font-medium text-gray-700 "
-                >
-                  タスク概要
-                </label>
-                <input
-                  {...register("taskTitle", { required: true })}
-                  className="w-full min-w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-                />
-                {errors.taskTitle && (
-                  <p className="text-red-400">必須項目です</p>
-                )}
-              </AppContainer>
-              <AppContainer>
-                <label
-                  htmlFor="my-input"
-                  className="mb-1 font-medium text-gray-700"
-                >
-                  タスク内容
-                </label>
-                <input
-                  {...register("taskContent", { required: true })}
-                  className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-                />
-                {errors.taskContent && (
-                  <p className="text-red-400">必須項目です</p>
-                )}
-              </AppContainer>
-              <button className="px-4 py-2 text-white bg-gray-500 rounded">
-                登録
-              </button>
-            </form>
-          </div>
-        </div>
-      )}
+      </div>
     </>
   );
 };
